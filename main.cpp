d24d0031f6cb9eceac3d7310ba22ab4113579d57
-rocrocket <-jakiewangseu@gmail.com>
2014-11-01 15:28:10 +0800

    v1.0

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+void main()
+{
+	char *s = "What a nice day!";
+	char *p = NULL;  //指向单词的开始
+	char *q = NULL;  //指向单词的结束
+	q = s;
+	while( * q) q ++;
+	q = q - 1;
+	p = q;
+	int i;
+	while(1)
+	{
+		while(p != s && *p != ' ')
+			 p = p - 1;
+		if(p == s)
+				{
+					for(i = 0; i <= q - p; i ++ )
+				    putchar( * (p+i));
+					break;
+		}
+		for(i = 1; i <= q - p; i++ )
+		putchar(*(p+i));
+		putchar(*p);
+		q = p - 1;
+		p = q;
+	}
+
+}
